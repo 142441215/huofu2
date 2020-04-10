bbcc5e2d089b4b1a34cb058ddd94bcf0a6556f6b
严浩 <1424412158@qq.com>
2020-04-10 15:16:45 +0800

    3

--- huofu2/huofu2.cpp
+++ huofu2/huofu2.cpp
@@ -1,20 +1,20 @@
-﻿// huofu2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
-
+﻿#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat srcMat = imread("c://project//smt.png");
+	Mat dstMat;
+	Canny(srcMat, dstMat, 50, 200, 3);
+	vector<Vec4i> lines;
+	HoughLinesP(dstMat, lines, 1, CV_PI / 180, 40, 50, 10);
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		Vec4i l = lines[i];
+		line(srcMat, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,255,0), 1, LINE_AA);
+	}
+	imshow("src", srcMat);
+	waitKey(0);
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
